777ce0cc944f00c22f9aaaf2530227bd572ccdd3
bamtercelboo <bamtercelboo@163.com>
2019-02-20 20:42:31 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 basepath=$(cd `dirname $0`; pwd)
 echo $basepath
 nohup python -u main.py --config ./Config/config.cfg > log 2>&1 &
