071ca80038263129e8a51c1b51fe867c43189574
bamtercelboo <bamtercelboo@163.com>
2019-02-20 16:01:40 +0800

    add tun.sh file

---
+++ run.sh
@@ -0,0 +1,10 @@
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export CUDA_VISIBLE_DEVICES=1
+basepath=$(cd `dirname $0`; pwd)
+echo $basepath
+nohup python -u main.py --config ./Config/config.cfg > log 2>&1 &
+tail -f log 
+ 
+
+
