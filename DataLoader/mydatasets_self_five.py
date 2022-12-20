38d26dbb9d71c3770e27e6703354f729dd95dacb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-20 14:02:19 +0000

    Adding tarfile member sanitization to extractall()

--- DataLoader/mydatasets_self_five.py
+++ DataLoader/mydatasets_self_five.py
@@ -37,7 +37,26 @@ class TarDataset(data.Dataset):
                 urllib.request.urlretrieve(cls.url, tpath)
             with tarfile.open(tpath, 'r') as tfile:
                 print('extracting')
-                tfile.extractall(root)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tfile, root)
         return os.path.join(path, '')
 
 
